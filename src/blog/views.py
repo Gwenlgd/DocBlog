535d6ae65ec12abd28579ecbb5015eb1509b9410
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2024-01-07 19:09:06 +0100

    Add: views articles

--- src/blog/views.py
+++ src/blog/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "blog/index.html")
+
+
+def article(request, numero_article):
+    if numero_article in ["01", "02", "03"]:
+        # f = f-string. Va permettre d'insérer des variables à l'intérieur de la chaîne de caractères
+        return render(request, f"blog/article_{numero_article}.html")
+    return render(request, "blog/article_not_found.html")
