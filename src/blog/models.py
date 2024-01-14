3da3d8121afeef69088ed1e59f021b0c9b2ed872
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2024-01-14 15:52:43 +0100

    Add: models

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -6,7 +7,20 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField()
     published = models.BooleanField(default=False)
-    date = models.DateField(blank=True)
+    date = models.DateField(blank=True, null=True)
     content = models.TextField()
     description = models.TextField()
+    category = models.TextField()
+
+    @property
+    def publish_string(self):
+        if self.published:
+            return "L'article est publié"
+        return "L'article est inacessible"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super().save(*args, **kwargs)
 
